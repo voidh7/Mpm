33733cc0746e5f99bb60ee28a4217c85dfe74e0a
Mr rvoid <vdev30301@gmail.com>
2025-05-24 19:21:01 -0300

    Create Mpm.c

---
+++ Mpm.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char nomes[5][50];
+    char links[5][100];
+} Bibliotecas;
+
+int main() {
+    Bibliotecas libs = {
+        .nomes = {
+            "GLib",
+            "libcurl",
+            "OpenSSL",
+            "SQLite",
+            "zlib"
+        },
+        .links = {
+            "https://download.gnome.org/sources/glib/2.74/glib-2.74.0.tar.xz",
+            "https://curl.se/download/curl-8.0.1.tar.gz",
+            "https://www.openssl.org/source/openssl-3.1.2.tar.gz",
+            "https://www.sqlite.org/2023/sqlite-autoconf-3420000.tar.gz",
+            "https://zlib.net/zlib-1.2.13.tar.gz"
+        }
+    };
+
+    char input[50];
+    printf("Digite o nome da biblioteca para baixar: ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    int encontrado = 0;
+    for(int i = 0; i < 5; i++) {
+        if (strcmp(input, libs.nomes[i]) == 0) {
+            encontrado = 1;
+            char cmd[200];
+
+            snprintf(cmd, sizeof(cmd), "curl -O %s", libs.links[i]);
+            printf("Baixando %s...\n", libs.nomes[i]);
+            int ret = system(cmd);
+            if (ret == -1) {
+                printf("Erro ao executar o comando de download.\n");
+            } else {
+                printf("Download finalizado.\n");
+            }
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        printf("Biblioteca não encontrada.\n");
+    }
+
+    return 0;
+}
